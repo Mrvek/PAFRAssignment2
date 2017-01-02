b7ca3475a08e462348d65fb83fc39cc2c0d2f5f7
Mitchell van Ek <mitchell.v.ek@xs4all.nl>
2017-01-02 16:33:37 +0100

    figured out empty packages arent send...

---
+++ poor-rail/src/paf/ass2/RichRail/Domain/Train.java
@@ -0,0 +1,7 @@
+package paf.ass2.RichRail.Domain;
+
+/**
+ * Created by Mitchell on 02/01/2017.
+ */
+public class Train {
+}
---
+++ poor-rail/src/paf/ass2/RichRail/GUI/PlaceholderThrowawayClass.java
@@ -0,0 +1,7 @@
+package paf.ass2.RichRail.GUI;
+
+/**
+ * Created by Mitchell on 02/01/2017.
+ */
+public class PlaceholderThrowawayClass {
+}
