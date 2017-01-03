fd67432ae99e3ed8d1f3cc1903c4f1044ade23f1
Jochem <jocokuus@gmail.com>
2017-01-03 12:13:25 +0100

    Added GUI basics

---
+++ Rich-rail/src/paf/ass2/RichRail/GUI/Main_View.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+/*
+ * Created by Jochem on 3-1-2017.
+ */
+public class Main_View {
+    private JPanel MainField;
+    private JButton commit;
+    private JTextArea textArea1;
+}
